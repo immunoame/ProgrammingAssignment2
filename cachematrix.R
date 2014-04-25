d0916a9e3fa6a57392265d36fb7266a3657dcdad
immunoame <dr.antibody@gmail.com>
2014-04-25 14:44:32 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## [Put comments here that describe what your functions do]
+##Amanda Enstrom PROGRAMMING ASSIGNMENT 2
 
-## Write a short comment describing this function
+###MakeCacheMatrix takes the inverse of a matrix _
+#matrix is cached for retrieval as retrieve
+
+###CacheSolve checks in matrix is cached
+#If matrix is cached, inverse is returned from memory
+#else inverse is solved & returned
 
 makeCacheMatrix <- function(x = matrix()) {
+  finder<-NULL ####does not yet exist
+  ###could use ginv() if available
+    ##Function 1 of 4 --> set --> look up the value in the parent env
+      set <- function(y) {
+        x <<- y
+        finder <<- NULL}       
+    ##Function 2 of 4 --> get --> retrieve the value from env
+      get <- function() x
+   ##Function 3 of 4 --> setinv (record inv matrix later retrieval _
+        ###using solve and set value)
+      setinv <- function(solve) finder <<- solve
+    ###Function 4 of 4 --> getinv --> retrieval
+      getinv <- function() finder
+    ###the functions to be called are stored as a list 
+      list(set = set, get = get, setinv = setinv,
+              getinv = getinv)}
 
-}
 
+  
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Look up matrix to see if inversed cached
+  ## Calculate if not cached
+  ##Return a matrix that is the inverse of 'x'
+  finder <- x$getinv()
+  
+  if(!is.null(finder)) {
+    message("getting cached data")
+    return(finder)
+  } ###else
+  data <- x$get()
+  finder <- solve(data, ...)
+  x$setinv(finder)
+  finder
 }
